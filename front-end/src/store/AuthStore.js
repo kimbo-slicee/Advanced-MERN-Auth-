dba973a1247ddd47f7959816a8944a8ee21b0d2a
mohammed el aouri <mohammedelaouri@gmail.com>
2024-12-30 01:24:13 +0100

    Commit message : `handle state management using Zustand library`

--- front-end/src/store/AuthStore.js
+++ front-end/src/store/AuthStore.js
@@ -1,7 +1,21 @@
 import { create } from 'zustand'
+import axios from 'axios'
 
+const API_URL="BACKEND_URL=http://localhost:5000/api/v1/auth"
 const useAuthStore = create((set) => ({
+user: null,
+isAuthenticated: false,
+isLoading:false,
+isCheckingAuth: false,
 
-
-
+singup: async (email, password,name) => {
+    set({ isLoading: true ,error:null})
+    try{
+        const response = await axios.post(`${API_URL}/signup`, {email, password,name});
+        set({user: response.data.data, isAuthenticated: true, isLoading: false})
+    }catch (error){
+    set({error: error.response.data.error || "SignUp Error", isLoading: false})
+    console.log(error)
+    }
+    }
 }))
