9dfa73ed5b39f4958e3cab6f00bde76307adadd5
mohammed el aouri <mohammedelaouri@gmail.com>
2025-01-05 22:23:42 +0100

    ...

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// Importing tailwindcss and autoprefixer using ES Module syntax
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
   },
 })
